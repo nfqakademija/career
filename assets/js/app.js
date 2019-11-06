321367b1298f22ac1daf15a8de2415095c7683d0
Matas <matas.jarilinas@stud.vgtu.lt>
2019-11-06 13:52:13 +0200

    added react

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,17 +1,12 @@
-/*
- * Welcome to your app's main JavaScript file!
- *
- * We recommend including the built version of this JavaScript file
- * (and its CSS file) in your base layout (base.html.twig).
- */
+ import React from 'react';
+ 
+const App = () => {
+    return (
+        <div>
+            <h1>React is added very cool</h1>
+            <h3>NFQ Career Criteria Assessment is in progress</h3>
+        </div>
+    )
+}
 
-// any CSS you require will output into a single css file (app.css in this case)
-require('../css/app.scss');
-
-// Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
-const $ = require('jquery');
-require('bootstrap');
-
-$(document).ready(function() {
-    $('[data-toggle="popover"]').popover();
-});
+export default App;
