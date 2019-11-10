f6e5d39b6a073e233217528777216abe8a1612f5
Andrius Stonys <29209107+AndriusSton@users.noreply.github.com>
2019-11-10 14:22:44 +0200

    added: fetch all criterias with choices (normalizer/encoder stuff added), changed: criteria fetching by competence title

--- src/Controller/CriteriaListController.php
+++ src/Controller/CriteriaListController.php
@@ -2,11 +2,17 @@
 
 namespace App\Controller;
 
+use App\Entity\Criteria;
 use FOS\RestBundle\Controller\AbstractFOSRestController;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use App\Repository\CriteriaRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Serializer;
 
 class CriteriaListController extends AbstractFOSRestController
 {
@@ -19,11 +25,35 @@ class CriteriaListController extends AbstractFOSRestController
     }
 
     /**
-     * @return \App\Entity\Criteria[]
+     * @return \FOS\RestBundle\View\View
      */
-
     public function getCriteriaListAction()
     {
-        return $this->criteriaRepository->fetchByCompetenceTitle('Experience');
+        $criteriaList = $this->criteriaRepository->findAll();
+        // Tip : Inject SerializerInterface $serializer in the controller method
+// and avoid these 3 lines of instanciation/configuration
+        $encoders = [new JsonEncoder()]; // If no need for XmlEncoder
+        $normalizers = [new ObjectNormalizer()];
+        $serializer = new Serializer($normalizers, $encoders);
+
+// Serialize your object in Json
+        $jsonObject = $serializer->serialize($criteriaList, 'json', [
+            'circular_reference_handler' => function ($object) {
+                return $object->getId();
+            }
+        ]);
+// For instance, return a Response with encoded Json
+        return new Response($jsonObject, Response::HTTP_OK, ['Content-Type' => 'application/json']);
+
     }
+
+    /**
+     * @param string $title
+     * @return Criteria[]
+     */
+    public function getCriteriaAction(string $title)
+    {
+        return $this->criteriaRepository->fetchByCompetenceTitle($title);
+    }
+
 }
