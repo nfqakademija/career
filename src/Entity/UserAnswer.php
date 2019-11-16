166a5dfbed962f27ed5cc5fb6a0aaec3f4ef43f3
Andrius Stonys <29209107+AndriusSton@users.noreply.github.com>
2019-11-16 14:58:27 +0200

    new entities created for answer gathering support

---
+++ src/Entity/UserAnswer.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\UserAnswerRepository")
+ */
+class UserAnswer
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\OneToOne(targetEntity="App\Entity\Criteria", cascade={"persist", "remove"})
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $fkCriteria;
+
+    /**
+     * @ORM\OneToOne(targetEntity="App\Entity\CriteriaChoice", cascade={"persist", "remove"})
+     */
+    private $fkChoice;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\CareerForm", inversedBy="userAnswers")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $fkCareerForm;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getFkCriteria(): ?Criteria
+    {
+        return $this->fkCriteria;
+    }
+
+    public function setFkCriteria(Criteria $fkCriteria): self
+    {
+        $this->fkCriteria = $fkCriteria;
+
+        return $this;
+    }
+
+    public function getFkChoice(): ?CriteriaChoice
+    {
+        return $this->fkChoice;
+    }
+
+    public function setFkChoice(?CriteriaChoice $fkChoice): self
+    {
+        $this->fkChoice = $fkChoice;
+
+        return $this;
+    }
+
+    public function getFkCareerForm(): ?CareerForm
+    {
+        return $this->fkCareerForm;
+    }
+
+    public function setFkCareerForm(?CareerForm $fkCareerForm): self
+    {
+        $this->fkCareerForm = $fkCareerForm;
+
+        return $this;
+    }
+}
