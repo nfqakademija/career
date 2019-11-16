9a81cd966eb81e20fb38b8a3852d23a29ef1dd2c
AinisSitkauskas <ainis795@gmail.com>
2019-11-16 16:58:44 +0200

    Add user manegment

--- config/bundles.php
+++ config/bundles.php
@@ -12,4 +12,5 @@ return [
     FOS\RestBundle\FOSRestBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     FOS\UserBundle\FOSUserBundle::class => ['all' => true],
+    Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
 ];
