f43438f9bb47c292027ef55cd998c12244de88d4
AinisSitkauskas <ainis795@gmail.com>
2019-11-23 18:27:27 +0200

    User table with maker bundle and Profession table

---
+++ src/Entity/Profession.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ProfessionRepository")
+ */
+class Profession
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $profession;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getProfession(): ?string
+    {
+        return $this->profession;
+    }
+
+    public function setProfession(string $profession): self
+    {
+        $this->profession = $profession;
+
+        return $this;
+    }
+}
