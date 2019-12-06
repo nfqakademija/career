9ccafb345760ef406df54f8266076681e116a36c
Andrius Stonys <29209107+AndriusSton@users.noreply.github.com>
2019-12-06 20:18:49 +0200

    endpoint description added

--- src/Controller/CareerFormController.php
+++ src/Controller/CareerFormController.php
@@ -30,6 +30,7 @@ use FOS\RestBundle\View\View;
  * If User does not have a career form assigned, create one by career profile;
  * /api/form/list - get career form list; TODO: get career form list by team;
  * /api/answers - post answer
+ * /api/answers/{slug} - get answers by form id
  *
  * @package App\Controller
  */
