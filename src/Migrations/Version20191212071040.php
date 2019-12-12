82ec6652dc1ae4ac6f59fe0592c677058169d53f
Andrius Stonys <29209107+AndriusSton@users.noreply.github.com>
2019-12-12 09:50:19 +0200

    migration version20191212071040 fix

--- src/Migrations/Version20191212071040.php
+++ src/Migrations/Version20191212071040.php
@@ -24,7 +24,7 @@ final class Version20191212071040 extends AbstractMigration
 
         $this->addSql('ALTER TABLE user CHANGE roles roles JSON NOT NULL');
         $this->addSql('ALTER TABLE manager_answer CHANGE is_valid_answer is_valid_answer TINYINT(1) DEFAULT NULL, CHANGE comment comment VARCHAR(500) DEFAULT NULL');
-        $this->addSql('ALTER TABLE career_profile ADD created_at DATETIME NOT NULL, ADD updated_at DATETIME DEFAULT NULL, DROP criteria_count');
+        $this->addSql('ALTER TABLE career_profile ADD created_at DATETIME NOT NULL, ADD updated_at DATETIME DEFAULT NULL, ADD criteria_count INT DEFAULT NULL');
         $this->addSql('ALTER TABLE user_answer CHANGE fk_choice_id fk_choice_id INT DEFAULT NULL, CHANGE comment comment VARCHAR(500) DEFAULT NULL, CHANGE updated_at updated_at DATETIME DEFAULT NULL');
         $this->addSql('ALTER TABLE career_form ADD updated_at DATETIME DEFAULT NULL, ADD under_evaluation TINYINT(1) NOT NULL');
     }
@@ -35,7 +35,7 @@ final class Version20191212071040 extends AbstractMigration
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('ALTER TABLE career_form DROP updated_at, DROP under_evaluation');
-        $this->addSql('ALTER TABLE career_profile ADD criteria_count INT DEFAULT NULL, DROP created_at, DROP updated_at');
+        $this->addSql('ALTER TABLE career_profile DROP created_at, DROP updated_at, DROP criteria_count');
         $this->addSql('ALTER TABLE manager_answer CHANGE is_valid_answer is_valid_answer TINYINT(1) DEFAULT \'NULL\', CHANGE comment comment VARCHAR(500) CHARACTER SET utf8mb4 DEFAULT \'NULL\' COLLATE `utf8mb4_unicode_ci`');
         $this->addSql('ALTER TABLE user CHANGE roles roles LONGTEXT CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_bin`');
         $this->addSql('ALTER TABLE user_answer CHANGE fk_choice_id fk_choice_id INT DEFAULT NULL, CHANGE comment comment VARCHAR(500) CHARACTER SET utf8mb4 DEFAULT \'NULL\' COLLATE `utf8mb4_unicode_ci`, CHANGE updated_at updated_at DATETIME DEFAULT \'NULL\'');
